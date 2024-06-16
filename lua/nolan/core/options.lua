7679d08b75d1c76d3a23a46d7b26e0cb035c35f5
NolanBeaujault <beaujaultnolan@gmail.com>
2024-06-17 00:46:57 +0200

    first commit

---
+++ lua/nolan/core/options.lua
@@ -0,0 +1,31 @@
+vim.cmd("let g:netrw_liststyle = 3")
+
+local opt = vim.opt
+
+opt.relativenumber = true
+opt.number = true
+
+-- tabs & indentation
+opt.tabstop = 2 -- 2 spaces for tabs (prettier default)
+opt.shiftwidth = 2 -- 2 spaces for indent width
+opt.expandtab = true -- expand tabs to spaces
+opt.autoindent = true -- copy indent from current line when starting a new line
+
+opt.wrap = false
+
+-- search settings
+opt.ignorecase = true
+opt.smartcase = true
+
+opt.cursorline = true
+
+opt.termguicolors = true
+opt.background = "dark"
+opt.signcolumn = "yes"
+
+opt.backspace = "indent,eol,start"
+
+opt.clipboard:append("unnamedplus")
+
+opt.splitright = true
+opt.splitbelow = true
